d23ef966a528fdecc931a7f7d5c291b389ff3905
Durotimi <timi@weprototype.co>
2023-01-12 21:29:39 +0100

    Added random methods

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Durotimi Johnson on 12/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        var diceArray = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"),UIImage(named: "DiceSix"),]
+        diceImageViewOne.image = diceArray[Int.random(in: 1...5)]
+        diceImageViewTwo.image = diceArray[Int.random(in: 1...5)]
+    }
+}
+
